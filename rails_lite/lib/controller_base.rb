10a78f378991a99e0c8fb7119268def7eab5f2f5
Kevin <kevinmao.xy@gmail.com>
2021-02-16 17:19:02 -0500

    finish part 1

--- rails_lite/lib/controller_base.rb
+++ rails_lite/lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -7,34 +8,67 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
+    @req = req
+    @res = res
+    @already_built_response = false
+    @params = req.params.merge(route_params)
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "double render" if already_built_response?
+    res.header['location'] = url
+    res.status = 302
+    @already_built_response = true
+    session.store_session(res)
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "double render" if already_built_response?
+    res['Content-Type'] = content_type
+    res.write(content)
+    @already_built_response = true
+    session.store_session(res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    controller_name = self.class.name.underscore
+    dir_path = File.dirname(__FILE__)
+    file_name = "#{template_name}.html.erb"
+    file_path = File.join(
+      dir_path,
+      "..",
+      "views",
+      controller_name,
+      file_name
+    )
+
+    template_content = File.read(file_path)
+
+    render_content(ERB.new(template_content).result(binding), "text/html")
+
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
   end
 end
 
